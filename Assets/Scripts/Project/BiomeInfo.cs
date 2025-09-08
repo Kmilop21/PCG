f4fd6cf3259a545604506ef22c55d8b00b52d41a
RainyGloom <rainhoom@gmail.com>
2025-09-07 23:07:30 -0300

    Change

---
+++ Assets/Scripts/Project/BiomeInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[Serializable]
+public struct BiomeInfo
+{
+    [NonSerialized] public Rect Area;
+    [field: SerializeField] public string Name { private set; get; }
+    [field: SerializeField] public GameObject[] Flora { private set; get; }
+    [field: SerializeField] public TerrainLayer TerrainLayer { private set; get; }  
+}
