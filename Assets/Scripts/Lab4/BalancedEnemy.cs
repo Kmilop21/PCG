4f97cd65b3c4f53cd40a5580c233b397c6cf393b
Camilo <112368065+Kmilop21@users.noreply.github.com>
2025-09-22 02:35:20 -0300

    Blanced enemy stat in UI

--- Assets/Scripts/Lab4/BalancedEnemy.cs
+++ Assets/Scripts/Lab4/BalancedEnemy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 
@@ -10,10 +11,12 @@ public class BalancedEnemy : MonoBehaviour
 {
     [SerializeField] private ESBalancedEnemySettler statsSettler;
     [NonSerialized] private Stats stats;
+    [SerializeField] private TextMeshProUGUI text;
 
     private void Start()
     {
         stats = statsSettler.GetBalancedStats();
         Debug.Log("Enemy: " + stats);
+        if(text != null) text.text = "Enemy stats: " + stats.ToString();
     }
 }
